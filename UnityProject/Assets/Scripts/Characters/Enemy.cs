791a8c95b2091045f03310eb48d52d695af77a33
Fernando <fgr1985@gmail.com>
2015-01-24 20:58:08 -0500

    added difficulty based logic for button prompts, also a fairly empty game manager script that is probably useless

--- UnityProject/Assets/Scripts/Characters/Enemy.cs
+++ UnityProject/Assets/Scripts/Characters/Enemy.cs
@@ -8,7 +8,7 @@ public class Enemy : MonoBehaviour
 	public float chaseWaitTime =5f;
 	public float patrolWaitTime =5f;
 	public EnemyTrigger enemyTrigger;
-	
+
 	public Transform[] patrolWayPoints;
 
 	private NavMeshAgent nav;
@@ -17,22 +17,66 @@ public class Enemy : MonoBehaviour
 	private float patrolTimer;
 	private int wayPointIndex;
 
+	//public int promptOne;
+	//public int promptTwo;
+	//public int promptThree;
+	
+	public int[] promptThresholds;
+	public string[] promptButtons;
+	public int promptIndex;
+	
+	public int promptTimer;
+
+	[SerializeField]
+	private int easyMin;
+	[SerializeField]
+	private int easyMax;
+	[SerializeField]
+	private int medMin;
+	[SerializeField]
+	private int medMax;
+	[SerializeField]
+	private int hardMin;
+	[SerializeField]
+	private int hardMax;
+
+	public enum difficultyStates 
+	{
+		EASY,
+		MEDIUM,
+		HARD
+	};
+	
+	string[] buttonList = new string[]
+	{
+		"A",
+		"B",
+		"X",
+		"Y",
+		"LB",
+		"RB",
+		"LSL",
+		"LSR",
+		"RSL",
+		"RSR",
+	};
+	
+	public	difficultyStates difficulty;
 	// Use this for initialization
 	void Start () 
 	{
-	
+		
 	}
 
 	void Awake() 
 	{
 		nav = GetComponent<NavMeshAgent> ();
-		wayPointIndex = Random.Range(0, patrolWayPoints.Length-1);
+		wayPointIndex = Random.Range(0, patrolWayPoints.Length);
 		//player = GameObject.FindGameObjectWithTag();
 	}
 	// Update is called once per frame
 	void Update () 
 	{
-
 		if (enemyTrigger.engage)
 			Engage ();
 		else if (enemyTrigger.exit)
@@ -40,6 +84,7 @@ public class Enemy : MonoBehaviour
 		else
 			Patrol ();
 	}
+	
 	void Engage()
 	{
 		nav.Stop ();
@@ -69,9 +114,79 @@ public class Enemy : MonoBehaviour
 		else
 			patrolTimer = 0f;
 	}
+	
 	void Exit ()
 	{
 		nav.speed = patrolSpeed * 2;
 		nav.SetDestination(enemyTrigger.exitPos);
+	}
+
+	public void EngageSet()
+	{
+		promptThresholds = new int[6];
+		promptButtons = new string[6];
+		promptIndex = 0;
+		
+		int holdCheck = Random.Range(0,2);
+
+		promptThresholds[promptIndex] = PromptRange();
+		promptButtons[promptIndex] = SetButtonPrompt();
+		
+		promptIndex++;
+				
+		if (difficulty == difficultyStates.MEDIUM || difficulty == difficultyStates.HARD)
+		{
+			promptButtons[promptIndex] = SetButtonPrompt();
+			promptThresholds[promptIndex] = PromptRange();
+			
+			promptIndex++;
+
+		}
+		else if (difficulty == difficultyStates.HARD)
+		{
+			promptButtons[promptIndex] = SetButtonPrompt();
+			promptThresholds[promptIndex] = PromptRange();
+		}
+	}
+	
+	int PromptRange()
+	{
+		int rangeCheck;
+		if (difficulty == difficultyStates.EASY)
+		{
+			return Random.Range(easyMin, easyMax);
+		}
+		else if(difficulty == difficultyStates.MEDIUM)
+		{
+			rangeCheck = Random.Range(0,3);
+			if (rangeCheck > 0)
+				return Random.Range(medMin, medMax);
+			else
+				return Random.Range(easyMin, easyMax);
+			//todo: hold prompts
+		}
+		else if(difficulty == difficultyStates.HARD)
+		{
+			rangeCheck = Random.Range(0,3);
+			if (rangeCheck > 0)
+				return Random.Range(hardMin, hardMax);
+			else
+				return Random.Range(medMin, medMax);
+			//todo: hold prompts
+		}
+		else
+			return 0;
+	}
+	string SetButtonPrompt()
+	{
+		return buttonList[Random.Range(0, buttonList.Length)];
+	}
+	
+	void SetHoldPrompts()
+	{
+		for (int i = promptIndex; i < 3; i++)
+		{
+			
+		}
 	}
 }
---
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	public int p1Score;
+	public int p2Score;
+	public int p3Score;
+	public int p4Score;
+
+	private int energy;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
