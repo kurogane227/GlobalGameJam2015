f0f2f34eea954d0a547fa6d0276452127183ffd6
David Blosser <blosserdw@gmail.com>
2015-01-24 21:00:56 -0500

    New button images, results screen functionality is in

---
+++ UnityProject/Assets/UIPanel_Results.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIPanel_Results : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	
+	public void OnBackButtonPressed()
+	{
+		Application.LoadLevel("mainScene");
+		UIManager.Instance.DestroyResults();
+	}
+}
