211e107c12c36524ee8d08ca4955d3f528785596
Nate Yelle <yelleNate@gmail.com>
2015-01-24 23:10:20 -0500

    physics!

---
+++ UnityProject/Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsObject : MonoBehaviour {
+
+	public Collider myCollider = null;
+
+
+	// Use this for initialization
+	void Start () {
+		if(myCollider!=null){
+			myCollider.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if(other.gameObject.tag == "Player"){
+			rigidbody.useGravity = true;
+			myCollider.enabled = true;
+		}
+	}
+}
